96f0e5f2de1ecefc47762e1e57333fc619c3927e
Administrator <dol.mouse92@gmail.com>
2018-03-21 13:51:53 +0700

    Tìm Giá trị trong mảng

--- NumberStudent.java
+++ NumberStudent.java
@@ -2,37 +2,21 @@ import java.util.Scanner;
 
 public class NumberStudent {
     public static void main(String[] args) {
-        int size; // kiem tra kick thuoc mang
-        int[] elements;
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
         Scanner sc = new Scanner(System.in);
-        do {
-            System.out.print("Enter a size:");
-            size = sc.nextInt();
-            if (size > 20)
-                System.out.println("Size does not exceed 20");
-        } while (size > 20);
+        System.out.print("Enter a name's student: ");
+        String input_name = sc.nextLine();
 
-        elements = new int[size]; //nhap gia tri cho cac phan tu cua mang
-        int i = 0;
-        while (i < elements.length) {
-            System.out.print("Enter element " + (i + 1) + ": ");
-            elements[i] = sc.nextInt();
-            i++;
+        boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(input_name)) {
+                System.out.println("Position of the students in the list " + "is " + (i + 1));
+                isExist = true;
+                break;
+            }
         }
+        if (!isExist)
+            System.out.println("Not found" + input_name + "in the list.");
 
-        System.out.printf("%-20s%s", "Elements in array: ", "");
-        for (int j = 0 ; j < elements.length; j++) {
-            System.out.print(elements[j] + "\t");
-        }
-
-        for (int j = 0;j < elements.length / 2; j++){
-            int temp = elements[j];
-            elements[j] = elements[size - 1 -j];
-            elements[size - 1 - j] = temp;
-        }
-        System.out.printf("%-20s%s", "Reverse array: ", "");
-        for (int j = 0; j < elements.length; j++) {
-            System.out.print(elements[j] + "\t");
-        }
     }
 }
